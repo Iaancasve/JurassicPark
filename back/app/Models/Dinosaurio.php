9f5b7dbf7c579460016e84a1ae61bd793f5c599b
Ian <iaancastellanos1234@gmail.com>
2026-03-05 15:47:04 +0100

    Añadir modelo de dinosaurio y migración con relación celda.

---
+++ back/app/Models/Dinosaurio.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Dinosaurio extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'especie',
+        'dieta',
+        'edad',
+        'estado_salud',
+        'celda_id'
+    ];
+
+    /**
+     * Obtener la celda a la que pertenece el dinosaurio.
+     */
+    public function celda(): BelongsTo
+    {
+        return $this->belongsTo(Celda::class);
+    }
+}
